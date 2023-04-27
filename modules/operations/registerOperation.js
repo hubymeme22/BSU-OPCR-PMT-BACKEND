7baa87ad548ecba69d64c409002bfe058d5645b3
HueHueberry <huby.new1112@gmail.com>
2023-04-27 10:57:15 +0800

    Registration for pmt accounts also added

--- modules/operations/registerOperation.js
+++ modules/operations/registerOperation.js
@@ -3,11 +3,32 @@ const Account = require('../../models/accounts');
 // registers a new head account
 module.exports.registerHead = async (username, password, res) => {
     const responseFormat = { registered: false, error: null };
-    const newAccount = new Account({
-        username: username,
-        password: password,
-        access: 'head',
-    });
+    try {
+        // checks if the account already exists
+        const account = await Account.findOne({ username: username });
+        if (account != null)
+            throw 'AccountAlreadyExist';
+
+        // otherwise, just save the new account
+        const newAccount = new Account({
+            username: username,
+            password: password,
+            access: 'head',
+        });
+
+        await newAccount.save();
+        responseFormat.registered = true;
+        res.json(responseFormat);
+
+    } catch (err) {
+        responseFormat.error = err;
+        res.json(responseFormat);
+    }
+};
+
+// registers a new pmt account
+module.exports.registerPMT = async (username, password, res) => {
+    const responseFormat = { registered: false, error: null };
 
     try {
         // checks if the account already exists
@@ -16,6 +37,12 @@ module.exports.registerHead = async (username, password, res) => {
             throw 'AccountAlreadyExist';
 
         // otherwise, just save the new account
+        const newAccount = new Account({
+            username: username,
+            password: password,
+            access: 'pmt',
+        });
+
         await newAccount.save();
         responseFormat.registered = true;
         res.json(responseFormat);
--- routes/register.js
+++ routes/register.js
@@ -12,9 +12,20 @@ registerRoute.use(midParam.paramCheckMiddle(['username', 'password']));
 
 // registers a new office head account
 registerRoute.post('/register/head', (req, res) => {
+    if (req.allowedDataError) return;
+
     // proceed to registering this account
     const { username, password } = req.body;
     routeOp.registerHead(username, password, res);
+});
+
+// registers a new pmt account
+registerRoute.post('/register/pmt', (req, res) => {
+    if (req.allowedDataError) return;
+
+    // proceed to registering this account
+    const { username, password } = req.body;
+    routeOp.registerPMT(username, password, res);
 });
 
 module.exports = registerRoute;
